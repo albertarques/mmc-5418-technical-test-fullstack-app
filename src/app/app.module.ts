18054a5682c03b61e79293555caa7625c9ee12e2
Albert Arques <albertarques@gmail.com>
2023-10-18 09:08:42 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { TransactionDetailComponent } from './transaction/transaction-detail/transaction-detail.component';
+import { TransactionListComponent } from './transaction/transaction-list/transaction-list.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { FooterComponent } from './shared/footer/footer.component';
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, TransactionDetailComponent, TransactionListComponent, UserDetailComponent, UserListComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/user/user-detail/user-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { UserListComponent } from '../user-list/user-list.component';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.scss']
+})
+export class UserDetailComponent {
+
+  // @Input() user!: User;
+
+}
