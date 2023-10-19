98fe7de746b746e9ca745521f182c0235ca86f1e
Albert Arques <albertarques@gmail.com>
2023-10-19 08:04:47 +0200

    Added Login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './transaction/login/login.component';
+
 const routes: Routes = [
   {
-    path: '',
-    component: AppComponent
+    path: '', component: AppComponent
   },
+  {
+    path: 'login', component: LoginComponent, pathMatch: 'full'
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { TransactionDetailComponent } from './transaction/transaction-detail/transaction-detail.component';
 import { TransactionListComponent } from './transaction/transaction-list/transaction-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserListComponent } from './user/user-list/user-list.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { UserComponent } from './user/service/user/user.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoginComponent } from './transaction/login/login.component';
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TransactionDetailComponent, TransactionListComponent, UserDetailComponent, UserListComponent, FooterComponent, UserComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TransactionDetailComponent,
+    TransactionListComponent,
+    UserDetailComponent,
+    UserListComponent,
+    FooterComponent,
+    LoginComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule
