caedd75e51935ceb4d88289b27f8ca4015021df4
Albert Arques <albertarques@gmail.com>
2023-10-22 18:47:27 +0200

    Renamed user to users folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Sared components
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
 import { TransactionDetailComponent } from './transactions/transaction-detail/transaction-detail.component';
 import { TransactionListComponent } from './transactions/transaction-list/transaction-list.component';
-import { UserDetailComponent } from './user/user-detail/user-detail.component';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { FooterComponent } from './shared/footer/footer.component';
+
+// Users components
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TransactionDetailComponent, TransactionListComponent, UserDetailComponent, UserListComponent, FooterComponent],
   imports: [
