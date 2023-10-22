5fed7281fa3df090f182c0c5ef49c1d693c10757
Albert Arques <albertarques@gmail.com>
2023-10-22 18:41:30 +0200

    Moved folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { TransactionDetailComponent } from './transaction/transaction-detail/transaction-detail.component';
 import { TransactionListComponent } from './transaction/transaction-list/transaction-list.component';
-import { UserDetailComponent } from './user/user-detail/user-detail.component';
-import { UserListComponent } from './user/user-list/user-list.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { LoginComponent } from './transaction/login/login.component';
 
